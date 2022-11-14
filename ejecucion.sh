37fc65bd614dcfc3c229effd82f53048db908e4e
ztira <yabmbs2001@gmail.com>
2022-11-14 10:02:14 +0100

    SH y gitignore creados

---
+++ ejecucion.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+echo "Empezando la primera ejecucion con 1 digito de nonce ->"
+
+./miner SGSSI-22.CB.03.txt 1
+
+echo "_________________________________________________________"
+
+echo "Empezando la primera ejecucion con 2 digito de nonce ->"
+
+./miner SGSSI-22.CB.03.txt 2
+
+echo "_________________________________________________________"
+
+echo "Empezando la primera ejecucion con 3 digito de nonce ->"
+
+./miner SGSSI-22.CB.03.txt 3
+
+echo "_________________________________________________________"
+
+echo "Empezando la primera ejecucion con 4 digito de nonce ->"
+
+./miner SGSSI-22.CB.03.txt 4
+
+echo "_________________________________________________________"
+
+echo "Empezando la primera ejecucion con 5 digito de nonce ->"
+
+./miner SGSSI-22.CB.03.txt 5
+
+echo "_________________________________________________________"
+
+echo "Empezando la primera ejecucion con 6 digito de nonce ->"
+
+./miner SGSSI-22.CB.03.txt 6
+
+echo "_________________________________________________________"
+
+echo "Empezando la primera ejecucion con 7 digito de nonce ->"
+
+./miner SGSSI-22.CB.03.txt 7
+
+echo "_________________________________________________________"
+
+echo "Empezando la primera ejecucion con 8 digito de nonce ->"
+
+./miner SGSSI-22.CB.03.txt 8
+
+echo "_________________________________________________________"
+
+echo "Empezando la primera ejecucion con 9 digito de nonce ->"
+
+./miner SGSSI-22.CB.03.txt 9
+
+echo "_________________________________________________________"
+
+echo "Empezando la primera ejecucion con 10 digito de nonce ->"
+
+./miner SGSSI-22.CB.03.txt 10
+
+echo "_________________________________________________________"
+
+exit
